ad9dd5684fd4e43d2a0c6e2a8f9b522ed514ca3c
LOXgames05 <130866295+LOXgames05@users.noreply.github.com>
2026-08-29 00:14:55 +0200

    config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  SUPABASE_URL: "https://kfheltqapgdoxazgjqe.supabase.co",
+  SUPABASE_URL: "https://kfheltqpapgdoxazgjqe.supabase.co",
   SUPABASE_PUBLISHABLE_KEY: "sb_publishable_ITXLQeaPTXhjfr2qA0iLaA_VY_cM5NI",
   MAP_IMAGE: "images/map.png"
 };
